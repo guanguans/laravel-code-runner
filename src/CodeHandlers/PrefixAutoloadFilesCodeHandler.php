0708c9aff5b4f3378e20dd2439cc9a99a064b107
yaozm <ityaozm@gmail.com>
2022-10-31 17:10:30 +0800

    Add `PrefixAutoloadFilesCodeHandler`

---
+++ src/CodeHandlers/PrefixAutoloadFilesCodeHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-code-runner.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\LaravelCodeRunner\CodeHandlers;
+
+use Illuminate\Support\Env;
+
+class PrefixAutoloadFilesCodeHandler
+{
+    public function handle(string $code, callable $next): string
+    {
+        $preloadFilesCode = '';
+
+        $autoloadFile = Env::get('VENDOR_DIR', base_path('vendor')).'/autoload.php';
+        if (file_exists($autoloadFile)) {
+            $preloadFilesCode = "require '$autoloadFile';".PHP_EOL;
+        }
+
+        $bootstrapFile = Env::get('BOOTSTRAP_FILE', base_path('bootstrap/app.php'));
+        if (file_exists($bootstrapFile)) {
+            $preloadFilesCode .= "require '$bootstrapFile';".PHP_EOL;
+        }
+
+        return $next($preloadFilesCode.$code);
+    }
+}
